e2ec4e3686ee5d6906b4a5b7c930b0728fb81818
jyfelt <JYFelt@163.com>
2018-03-01 15:02:28 +0800

    new

---
+++ 0000-排序/day0000-比较排序/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+# @Project : Algorithm_Python
+# @Time    : 0301
+# @Author  : Shylock
+# @Email   : JYFelt@163.com
+# @File    : __init__.py.py
+# @Software: PyCharm
+# ----------------------------------------------------
+# import something
---
+++ 0000-排序/day0000-比较排序/比较排序.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+# @Project : Algorithm_Python
+# @Time    : 0301
+# @Author  : Shylock
+# @Email   : JYFelt@163.com
+# @File    : 比较排序.py
+# @Software: PyCharm
+# ----------------------------------------------------
+# import something
+from random import randint
+
+# 梳理排序，是基于冒泡排序的一种改进：
+
+# 冒泡排序：
+my_list = []
+[my_list.append(randint(0, 100)) for x in range(10)]
+print(my_list)
+
+for i in range(1, len(my_list) - 1):
+    for j in range(1, len(my_list) - i):
+        if my_list[j] > my_list[j + 1]:
+            my_list[j], my_list[j + 1] = my_list[j + 1], my_list[j]
+
+print(my_list)
