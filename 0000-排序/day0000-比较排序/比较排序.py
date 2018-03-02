0be131cd172485d70283c52c776a3518b71b6477
jyfelt <JYFelt@163.com>
2018-03-02 22:44:32 +0800

    new

--- 0000-排序/day0000-比较排序/比较排序.py
+++ 0000-排序/day0000-比较排序/比较排序.py
@@ -9,13 +9,30 @@
 # import something
 from random import randint
 import math
+import time
+from functools import wraps
 
 # 冒泡排序：
 my_list = []
-[my_list.append(randint(0, 100)) for x in range(10)]
+[my_list.append(randint(0, 1000)) for x in range(1000)]
 print('原数据\n', my_list)
 
 
+def fn_timer(func):
+    @wraps(func)
+    def function_timer(*args, **kwargs):
+        t0 = time.time()
+        result = func(*args, **kwargs)
+        t1 = time.time()
+        print("Total time running %s: %s seconds" %
+              (func.__name__, str(t1 - t0))
+              )
+        return result
+
+    return function_timer
+
+
+@fn_timer
 def bubble_sort(num_list):
     """
     冒泡排序的特点就是调整相邻两个对象的位置，每进行一次内循环，就可以将最大值调整到最后，
@@ -31,15 +48,37 @@ def bubble_sort(num_list):
     return num_list
 
 
-print('经过冒泡排序后的数据\n', bubble_sort(my_list))
-
-
-
 # 梳理排序
-def comb_sort(num_list):
+
+@fn_timer
+def comb_sort(num_list, flag=False, s=1.3):
     """
     梳理排序，是基于冒泡排序的一种改进：
+    :param s:
+    :param flag:
     :param num_list:
     :return:
     """
-    pass
+    j = len(num_list)-1
+    while (j > 1) or flag is True:
+        i = 0
+        j = max(math.floor(j / s), 1)
+        flag = False
+        while i + j <= len(num_list)-1:
+            if num_list[i-1] > my_list[i + j-1]:
+                num_list[i - 1], num_list[i + j - 1] = num_list[i + j - 1], num_list[i - 1]
+                flag = True
+            i = i + 1
+
+    return num_list
+
+
+@fn_timer
+def py_sort(num_list):
+    return sorted(num_list)
+
+
+if __name__ == '__main__':
+    print('经过冒泡排序后的数据\n', bubble_sort(my_list))
+    print('经过梳理排序后的数据\n', comb_sort(my_list))
+    print('经过sorted函数排序后的数据\n', py_sort(my_list))
