550a554e7f7fb173b6c67276404fdad3e5f0f4c4
jyfelt <JYFelt@163.com>
2018-03-03 00:27:41 +0800

    new

--- 0000-排序/day0000-比较排序/比较排序.py
+++ 0000-排序/day0000-比较排序/比较排序.py
@@ -32,24 +32,25 @@ def fn_timer(func):
     return function_timer
 
 
+# 冒泡排序
 @fn_timer
-def bubble_sort(num_list):
+def bubble_sort(lists):
     """
     冒泡排序的特点就是调整相邻两个对象的位置，每进行一次内循环，就可以将最大值调整到最后，
     这样以后就可以不必再考虑它了，在进行了n-1次内循环后，就得到了完整的有序列，
-    时间复杂度为O(n^2)，虽然时间上不占优势，但是代码非常简洁，实现难度很低。。
-    :param num_list:
-    :return: sorted list
+    时间复杂度为O(n^2)，虽然时间上不占优势，但是代码非常简洁，实现难度很低。
+    :param lists:
+    :return:
     """
-    for i in range(len(num_list) - 1):
-        for j in range(1, len(num_list) - i):
-            if num_list[j - 1] > num_list[j]:
-                num_list[j], num_list[j - 1] = num_list[j - 1], num_list[j]
-    return num_list
+    count = len(lists)
+    for i in range(0, count):
+        for j in range(i + 1, count):
+            if lists[i] > lists[j]:
+                lists[i], lists[j] = lists[j], lists[i]
+    return lists
 
 
 # 梳理排序
-
 @fn_timer
 def comb_sort(num_list, flag=False, s=1.3):
     """
